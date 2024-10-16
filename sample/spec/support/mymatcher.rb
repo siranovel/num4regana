3f169bf1ae3ca4776eb6ca34fa29ec1f9c542f9e
siranovel <24727362+siranovel@users.noreply.github.com>
2024-10-16 11:45:11 +0900

    Add files via upload

--- sample/spec/support/mymatcher.rb
+++ sample/spec/support/mymatcher.rb
@@ -1,9 +1,11 @@
 require_relative('mylineregmatcher')
 require_relative('myroundmatcher')
+require_relative('myisarrmatcher')
 
 RSpec.configure do |config|
   config.include MyLineRegMatcher
   config.include MyRoundMatcher
+  config.include MyIsArrMatcher
 end
 
 
