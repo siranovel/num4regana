c6b6e6b04bee3dc5469e7f6642127f02ba335431
siranovel <24727362+siranovel@users.noreply.github.com>
2025-05-06 09:04:35 +0900

    Add files via upload

--- lib/num4effctual.rb
+++ lib/num4effctual.rb
@@ -11,10 +11,19 @@ module Num4EffectualLib
         def initialize
             @effect = Effectual.getInstance()
         end
+        # 単回帰による効果推定
         def smple_line_reg_ana(yi, xi)
             regana = Num4LineRegAnaLib::SmplRegAnaLib.new
             ret = regana.line_reg_ana(yi, xi)
             return ret[:slope]
+        end
+        # 傾向スコアによる効果推定
+        #   傾向スコアマッチング(PSM)
+#        def psm
+#        end
+        #   逆確率重み付け推定(IPW)
+        def ipw(yi, xij, zi)
+            return @effect.ipw(yi.to_java(Java::double), xij.to_java(Java::double[]), zi.to_java(Java::double))
         end
     end
 end
