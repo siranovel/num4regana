a7adca316fca231562acb1432dedc044364cae41
siranovel <24727362+siranovel@users.noreply.github.com>
2025-05-06 09:05:51 +0900

    Add files via upload

--- sample/spec/effect_spec.rb
+++ sample/spec/effect_spec.rb
@@ -1,7 +1,45 @@
 require 'spec_helper'
 require 'num4regana'
+require 'csv'
+
+class EffectDat
+    def initialize
+        csv_dt = CSV.read('bias_df.csv')
+        # Yi: spend
+        # Zi: treatment
+        @yi = []
+        @xij = []
+        @zi = []
+        csv_dt.each do |dt|
+            @yi.push(dt[11].to_i)           # spend
+            @zi.push(dt[12].to_i)           # treatment
+            case dt[7]                 # channel(Web, Phone, Multichannel)
+            when "Phone"               # recency + history + channel
+                @xij.push([dt[0].to_i, dt[2].to_f, 1, 0])
+            when "Web"
+                @xij.push([dt[0].to_i, dt[2].to_f, 0, 1])
+            when "Multichannel"
+                @xij.push([dt[0].to_i, dt[2].to_f, 1, 1])
+            else
+                @xij.push([dt[0].to_i, dt[2].to_f, 0, 0])
+            end
+        end
+    end
+    def yi
+        return @yi
+    end
+    def xij
+        return @xij
+    end
+    def zi
+        return @zi
+    end
+end
 
 RSpec.describe Num4RegAnaLib do
+    before(:all) do
+        @bias_t = EffectDat.new
+    end
     describe Num4RegAnaLib::RCTLib do
         let!(:regana) { Num4RegAnaLib::RCTLib.new }
         it '#smple_line_reg_ana 1' do
@@ -17,6 +55,16 @@ RSpec.describe Num4RegAnaLib do
             expect(
                 regana.smple_line_reg_ana(yi, zi)
             ).to my_round(100.0, 1)
+        end
+        it '#ipw' do
+            yi = @bias_t.yi
+            xij = @bias_t.xij
+            zi = @bias_t.zi
+            expect(
+                regana.ipw(yi, xij, zi)
+            ).to my_round(100.0, 1)
+
+            p @bias_t.xi[0]
         end
     end
 end
