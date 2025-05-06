08dfa63fcd670b486bbdeed31b2f44a50803500f
siranovel <24727362+siranovel@users.noreply.github.com>
2025-05-06 09:03:59 +0900

    Add files via upload

--- ext/num4regana/Effectual.java
+++ ext/num4regana/Effectual.java
@@ -3,6 +3,28 @@ public class Effectual {
     public static Effectual getInstance() {
         return effect;
     }
+    public double ipw(double[] yi, double[][] xij, double[] zi) {
+        IPW ps_ipw = new IPW();
+
+        return ps_ipw.ipw(yi, xij, zi);
+    }
+    /*********************************/
+    /* interface define              */
+    /*********************************/
+    /*********************************/
+    /* class define                  */
+    /*********************************/
+    private class IPW {
+        LogitRegAna logitReg = LogitRegAna.getInstance();
+        public double ipw(double[] yi, double[][] xij, double[] zi) {
+            MultLineReg lineReg = logitReg.nonLineRegAna(zi, xij);
+
+            return 0.0;
+        }
+        private double logitFunc(MultLineReg lineReg, double[] xi) {
+            return 0.0;
+        }
+    }
 }
 
 
