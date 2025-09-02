b5e493ae7cb2c7728038dcd560f088f523c08c1b
siranovel <24727362+siranovel@users.noreply.github.com>
2025-09-02 11:01:38 +0900

    Add files via upload

--- lib/num4effctual.rb
+++ lib/num4effctual.rb
@@ -61,6 +61,16 @@ module Num4EffectualLib
         def did(yi, ti, zi)
             return @effect.did(yi.to_java(Java::double), ti.to_java(Java::double),zi.to_java(Java::double)) 
         end
+        # 回帰不連続デザイン(Regreion Discontinuity Design:RDD)
+        #
+        # @overload rdd(yi, xi, zi)
+        #   @param [Array] yi 目的変数(double[])
+        #   @param [Array] xi 共変量変数(double[])
+        #   @param [Array] zi 介入変数(double[])
+        #   @return [double] 効果量
+        def rdd(yi, xi, zi)
+            return @effect.rdd(yi.to_java(Java::double), xi.to_java(Java::double), zi.to_java(Java::double))
+        end
     end
 end
 
